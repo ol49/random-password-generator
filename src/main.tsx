d6f12d4176bc8c824c8c52e2e6176be0c62b8101
Potato <discodingpotato@gmail.com>
2024-12-06 00:54:11 +0530

    fix: enable dark theme globally (nextuiprovoder)

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { NextUIProvider } from '@nextui-org/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NextUIProvider>
+    <NextUIProvider className='dark'>
       <App />
     </NextUIProvider>
   </StrictMode>,
