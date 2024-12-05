c6a00114a2f17624cfc8699e48d58cc049079635
Potato <discodingpotato@gmail.com>
2024-12-05 19:05:53 +0530

    initial: base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white h-screen flex items-center justify-center">
+        Random Password Generator
+      </div>
+    </>
+  )
+}
+
+export default App
